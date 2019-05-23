0db23fa3e8097a02e2693b2d3146c3952ce273bb
brett <brett_matsumoto@yahoo.com>
2019-05-22 15:05:06 -1000

    4-form-validation

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -19,7 +19,61 @@ export class ContactComponent {
 
   constructor() {}
 
+  nameInvalid = true;
+  nameErrorMessage = '';
+  emailInvalid = true;
+  emailErrorMessage = '';
+  messageInvalid = true;
+  messageErrorMessage = '';
+
+  validateName() {
+    const { name } = this.formData;
+
+    if (!name) {
+      this.nameErrorMessage = 'Name is Required';
+      return this.nameInvalid = true;
+    }
+    else if (name.length < 3) {
+      this.nameErrorMessage = 'Name is too short';
+      return this.nameInvalid = true;
+    }
+    this.nameErrorMessage = '';
+    return this.nameInvalid = false;
+  }
+
+  validateEmail() {
+    const { email } = this.formData;
+
+    if(!email) {
+      this.emailErrorMessage = 'Email is Required';
+      return this.emailInvalid = true;
+    }
+    else if (!email.includes('@')) {
+      this.emailErrorMessage = 'Email Not Formatted Correctly';
+      return this.emailInvalid = true;
+    }
+
+    this.emailErrorMessage = '';
+    return this.emailInvalid = false;
+  }
+
+  validateMessage() {
+    const { message } = this.formData;
+
+    if (!message) {
+      this.messageErrorMessage = 'Message is Required';
+      return this.messageInvalid = true;
+    }
+
+    this.messageErrorMessage = '';
+    return this.messageInvalid = false;
+  }
+
   submit() {
-    console.log(this.formData); 
+    if (this.nameInvalid || this.emailInvalid || this.messageInvalid) {
+      return;
+    }
+
+    console.log(this.formData);
   }
 }
